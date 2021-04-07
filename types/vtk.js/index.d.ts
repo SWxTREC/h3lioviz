55b61c82a55e4a5ea15600aad8ba6688f4941524
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2021-04-06 19:22:43 -0600

    remove local d.ts declaration files

--- types/vtk.js/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
-// declare module 'vtk.js/Sources/Rendering/Core/RenderWindow';
-// declare module 'vtk.js/Sources/Rendering/Core/RenderWindowInteractor';
-// declare module 'vtk.js/Sources/Rendering/Core/Renderer';
-declare module 'vtk.js/Utilities/config/dependency';
