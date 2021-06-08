0bbc9176d884f1dd9e7ae6f86abe79ec74e18f4a
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2021-06-07 20:48:21 -0600

    add control panel and interact with enlil view

--- src/app/containers/visualizer/visualizer.container.ts
+++ src/app/containers/visualizer/visualizer.container.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { snakeCase } from 'lodash';
+import { debounceTime } from 'rxjs/operators';
 import vtkWSLinkClient from 'vtk.js/Sources/IO/Core/WSLinkClient';
 import vtkRemoteView, {
     connectImageStream
@@ -12,8 +15,23 @@ import SmartConnect from 'wslink/src/SmartConnect';
 })
 export class VisualizerComponent implements AfterViewInit {
     @ViewChild('pvContent', { read: ElementRef }) pvContent: ElementRef;
+    controlPanel = new FormGroup({
+        bvec: new FormControl(false),
+        cme: new FormControl(false),
+        data: new FormControl(false),
+        latSlice: new FormControl(true),
+        lonArrows: new FormControl(false),
+        lonSlice: new FormControl(false),
+        lonStreamlines: new FormControl(true),
+        magneticFields: new FormGroup({
+            x: new FormControl({ value: false, disabled: true }),
+            y: new FormControl({ value: false, disabled: true }),
+            z: new FormControl({ value: false, disabled: true })
+        })
+    });
+    isIndeterminate: { [parameter: string]: boolean } = {};
     pvView: any;
-    zoomState: 'on' | 'off' = 'off';
+    zoomState: 'on' | 'off' = 'on';
 
     ngAfterViewInit(): void {
         vtkWSLinkClient.setSmartConnectClass(SmartConnect);
@@ -50,7 +68,7 @@ export class VisualizerComponent implements AfterViewInit {
         // hint: if you use the launcher.py and ws-proxy just leave out sessionURL
         // (it will be provided by the launcher)
         const config = {
-            application: 'cone',
+            application: 'enlil',
             sessionURL: 'ws://localhost:1234/ws'
         };
 
@@ -63,11 +81,47 @@ export class VisualizerComponent implements AfterViewInit {
                 const session = validClient.getConnection().getSession();
                 this.pvView.setSession(session);
                 this.pvView.setViewId(-1);
-                this.pvView.render();
+                if ( this.zoomState === 'on' ) {
+                    this.pvView.setRpcWheelEvent('viewport.mouse.zoom.wheel');
+                }
+                this.updateControls( this.controlPanel.value );
             })
             .catch((error: any) => {
                 console.error(error);
             });
+
+        this.controlPanel.valueChanges.pipe( debounceTime(300) ).subscribe( newFormValues => {
+            this.updateControls( newFormValues );
+        });
+    }
+
+    determineChildStatus( parentControlName: string, childGroup: string ) {
+        const group = this.controlPanel.controls[childGroup] as FormGroup;
+        // give the checkbox a beat to change its status
+        setTimeout(() => {
+            const newValue: boolean = this.controlPanel.value[parentControlName];
+            // if parent is toggled, children should match its state
+            Object.keys( group.controls ).forEach( control => {
+                group.controls[control].setValue(newValue);
+            });
+        }, 0);
+    }
+
+    determineParentStatus( parentControlName: string, childGroup: string ) {
+        const group = this.controlPanel.controls[childGroup] as FormGroup;
+        // give the checkbox a beat to change its status
+        setTimeout(() => {
+            // check if all, none, or some of the parent control's child group are checked
+            if (Object.values(group.controls).every(control => control.value === true)) {
+                this.isIndeterminate[parentControlName] = false;
+                this.controlPanel.controls.bvec.setValue(true);
+            } else if (Object.values(group.controls).every(control => !control.value )) {
+                this.isIndeterminate[parentControlName] = false;
+                this.controlPanel.controls.bvec.setValue(false);
+            } else {
+                this.isIndeterminate[parentControlName] = true;
+            }
+        }, 0);
     }
 
     toggleZoom() {
@@ -80,5 +134,17 @@ export class VisualizerComponent implements AfterViewInit {
             this.zoomState = 'on';
             this.pvView.setRpcWheelEvent('viewport.mouse.zoom.wheel');
         }
+    }
+
+    updateControls( controlStates: { [parameter: string]: any; } ) {
+        const session = this.pvView.get().session;
+        Object.keys(controlStates).forEach( control => {
+            const state = controlStates[control] === true ? 'on' : 'off';
+            const name = snakeCase(control);
+            if ( typeof controlStates[control] === 'boolean') {
+                session.call('pv.enlil.visibility', [ name, state ]);
+            }
+        });
+        this.pvView.render();
     }
 }
