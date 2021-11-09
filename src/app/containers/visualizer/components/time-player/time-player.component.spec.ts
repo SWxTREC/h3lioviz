7fc9e68522d77b459513d117991a3fd78b62fb28
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2021-11-09 11:00:04 -0700

    fix linting

--- src/app/containers/visualizer/components/time-player/time-player.component.spec.ts
+++ src/app/containers/visualizer/components/time-player/time-player.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TimePlayerComponent } from './time-player.component';
 
 describe('TimePlayerComponent', () => {
-  let component: TimePlayerComponent;
-  let fixture: ComponentFixture<TimePlayerComponent>;
+    let component: TimePlayerComponent;
+    let fixture: ComponentFixture<TimePlayerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TimePlayerComponent ]
-    })
+    beforeEach( async() => {
+        await TestBed.configureTestingModule({
+            declarations: [ TimePlayerComponent ]
+        })
     .compileComponents();
-  });
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TimePlayerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TimePlayerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
