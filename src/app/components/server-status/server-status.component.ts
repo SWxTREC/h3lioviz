932c15fc61d1e9e454587a820c387471da1c24bd
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2022-01-26 16:11:44 -0700

    remove logged in status from server status component

--- src/app/components/server-status/server-status.component.ts
+++ src/app/components/server-status/server-status.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
-import { AwsService, ProfileNavService } from 'src/app/services';
+import { AwsService } from 'src/app/services';
 
 @Component({
     selector: 'swt-server-status',
@@ -9,30 +9,15 @@ import { AwsService, ProfileNavService } from 'src/app/services';
     styleUrls: [ './server-status.component.scss' ]
 })
 export class ServerStatusComponent implements OnDestroy {
-    isLoggedIn: boolean;
     serverStarted: boolean;
     subscriptions: Subscription[] = [];
-    serverStartedSubscription: Subscription;
 
     constructor(
-        private profileService: ProfileNavService,
         private awsService: AwsService
     ) {
-        this.subscriptions.push( this.profileService.isLoggedIn.pipe( distinctUntilChanged() )
-        .subscribe( (loginStatus: boolean) => {
-            this.isLoggedIn = loginStatus;
-            if ( loginStatus ) {
-                this.serverStartedSubscription = this.awsService.pvServerStarted$.pipe( distinctUntilChanged() )
-                .subscribe( ( status: boolean ) => {
-                    this.serverStarted = status;
-                });
-                this.subscriptions.push( this.serverStartedSubscription );
-            } else {
-                if ( this.serverStartedSubscription ) {
-                    this.serverStartedSubscription.unsubscribe();
-                }
-            }
-        }));
+        this.subscriptions.push( this.awsService.pvServerStarted$.pipe( distinctUntilChanged() )
+            .subscribe( ( status: boolean ) => this.serverStarted = status )
+        );
     }
 
     ngOnDestroy(): void {
