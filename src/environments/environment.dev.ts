da382667e08762a260361f9bb2c45f51fb698884
Greg Lucas <greg.m.lucas@gmail.com>
2022-08-02 16:24:50 -0600

    ENH: Move to one Cognito instance
    Copy the Cognito login info from prod over to dev so we use
    that instead of new Cognito instances all the time.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -16,11 +16,11 @@ export const environment = {
     aws: {
         api: 'https://apigw.dev.swx-trec.com/h3lioviz',
         cognito: {
-            appClientId: '7tefs84hbme9m5etvg34h1hen',
+            appClientId: '5itqpae8gseickjbemm5tprpef',
             region: 'us-east-1',
-            userPoolId: '1aA5ZzW8s',
-            loginPage: 'https://dev-swx-trec.auth.us-east-1.amazoncognito.com',
-            identityPoolId: 'c6ff4d3a-226d-4a14-bcac-c32e8f2a7c84'
+            userPoolId: 'Dgc4otkxZ',
+            loginPage: 'https://swx-trec.auth.us-east-1.amazoncognito.com',
+            identityPoolId: '9f26842f-2e5d-4c32-abf5-91b71e82e3a2'
         }
     },
     production: true,
