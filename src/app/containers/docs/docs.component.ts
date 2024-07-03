b1a90d354539d972a0c97ed649d10bfa1d1a70fd
Ransom Christofferson <ransom.christofferson@lasp.colorado.edu>
2024-07-03 22:09:55 +0000

    Applied suggestion

--- src/app/containers/docs/docs.component.ts
+++ src/app/containers/docs/docs.component.ts
@@ -9,7 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class DocsComponent implements OnInit {
     keyboardShortcuts: IKeyboard[] = KEYBOARD_SHORTCUTS;
-    isDev = environment.dev === true;
+    isDev = environment.dev;
     moreInfoHref = `https://${this.isDev ? 'dev' : ''}.swx-trec.com`;
 
     constructor() { }
