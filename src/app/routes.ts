0bac8fec08feba8c0cbdd72689183fa631024296
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2025-04-03 16:44:55 -0600

    add page titles

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,13 +4,16 @@ import { FooterMode } from 'lasp-footer';
 export const routes: Routes = [
     {
         path: '',
+        title: 'H3lioViz: Interactive 3D Heliosphere Visualizer',
         loadChildren: () => import( './containers/home/home.module').then( m => m.HomeModule )
     }, {
         path: 'docs',
+        title: 'Documentation | H3lioViz',
         loadChildren: () => import( './containers/docs/docs.module').then( m => m.DocsModule )
     }, {
         // canActivate: [ AuthGuard ],
         data: { footer: FooterMode.MINIMAL },
+        title: 'Visualizer | H3lioViz',
         loadChildren: () => import( './containers/visualizer/visualizer.module').then( m => m.VisualizerModule ),
         path: 'visualizer'
     }, {
